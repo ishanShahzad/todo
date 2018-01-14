1bf5bf62777203dea9c2f30ba7b96ec28da7b8c0
ishanShahzad <3006761319ishan@gmail.com>
2018-01-14 15:01:34 +0500

    initial release

---
+++ file1.js
@@ -0,0 +1,10 @@
+
+var moment = require('moment')
+var z = require('./file2')
+
+var a=1;
+console.log('...loadingfiles' , a , z)
+var date = moment().format('DD:MM:YYYY');
+
+console.log(typeof date);
+
---
+++ file2.js
@@ -0,0 +1,7 @@
+var b = 2;
+console.log('....file2 loading', b)
+
+
+
+
+//module.exports=testb;
